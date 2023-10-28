1e4c9dffa0fde3585f8fed0ce71f32b5fc816a80
Codreanu Alexandru <alovada.med@gmail.com>
2023-10-28 20:03:58 +0300

    user manual

--- data/mainwindow.cpp
+++ data/mainwindow.cpp
@@ -324,7 +324,7 @@ void MainWindow::openSourceCode()
 
 void MainWindow::openUserManual()
 {
-    QDesktopServices::openUrl(QUrl("https://github.com/debalex77/USG/wiki/User-Manual-(ro)"));
+    QDesktopServices::openUrl(QUrl("https://debalex77.github.io/usg_user_manual"));
 }
 
 void MainWindow::openHistoryVersion()
